5499257487f1c2d77bf0036bbe40d66946eccd75
zarlex <alex.zarges@online.de>
2016-11-15 23:27:07 +0100

    extend class from base BackboneJS model and configure it

--- app/modules/heroes/models/hero.model.ts
+++ app/modules/heroes/models/hero.model.ts
@@ -1,4 +1,20 @@
-export class Hero {
-    id: number;
-    name: string;
+import {Injectable} from '@angular/core';
+import {BaseModel} from '../../backbone/models/backbone.model';
+
+@Injectable()
+export class Hero extends BaseModel {
+    urlRoot = 'app/heroes';
+
+    defaults(){
+        return {
+            name: ''
+        };
+    }
+
+    validate(attrs: any){
+        attrs.name = attrs.name.trim();
+        if (!attrs.name) {
+            return 'Name is required';
+        }
+    }
 }
